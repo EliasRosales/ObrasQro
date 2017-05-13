8bd4879e9c7e6bae0050bde19a84f8cf3f3db43e
Elias Rosales <mansion1@hotmail.com>
2017-05-13 16:22:28 -0500

    Interfaz Terminada

--- app/src/main/java/com/claresti/obrasqro/Main.java
+++ app/src/main/java/com/claresti/obrasqro/Main.java
@@ -1,7 +1,15 @@
 package com.claresti.obrasqro;
 
+import android.content.Intent;
+import android.support.annotation.NonNull;
+import android.support.design.widget.NavigationView;
 import android.support.v4.app.FragmentActivity;
 import android.os.Bundle;
+import android.support.v4.widget.DrawerLayout;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.view.View;
+import android.widget.ImageView;
 
 import com.google.android.gms.maps.CameraUpdateFactory;
 import com.google.android.gms.maps.GoogleMap;
@@ -10,8 +18,18 @@ import com.google.android.gms.maps.SupportMapFragment;
 import com.google.android.gms.maps.model.LatLng;
 import com.google.android.gms.maps.model.MarkerOptions;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class Main extends FragmentActivity implements OnMapReadyCallback {
 
+    //Menu, Declaracion de variables
+    private DrawerLayout drawerLayout;
+    final List<MenuItem> items = new ArrayList<>();
+    private Menu menu;
+    private ImageView btnMenu;
+    private NavigationView nav;
+    //Variable mapa
     private GoogleMap mMap;
 
     @Override
@@ -22,8 +40,14 @@ public class Main extends FragmentActivity implements OnMapReadyCallback {
         SupportMapFragment mapFragment = (SupportMapFragment) getSupportFragmentManager()
                 .findFragmentById(R.id.map);
         mapFragment.getMapAsync(this);
-    }
 
+        //Menu, Inicia las variables del menu y llama la funcion encargada de su manipulacion
+        drawerLayout = (DrawerLayout) findViewById(R.id.dLayout);
+        nav = (NavigationView)findViewById(R.id.navigation);
+        menu = nav.getMenu();
+        menuNav();
+
+    }
 
     /**
      * Manipulates the map once available.
@@ -43,4 +67,56 @@ public class Main extends FragmentActivity implements OnMapReadyCallback {
         mMap.addMarker(new MarkerOptions().position(sydney).title("Marker in Sydney"));
         mMap.moveCamera(CameraUpdateFactory.newLatLng(sydney));
     }
+
+    /**
+     * Funcion que da funcionalidad al menu
+     */
+    private void menuNav() {
+        for (int i = 0; i < menu.size(); i++) {
+            items.add(menu.getItem(i));
+        }
+        items.get(0).setChecked(true);
+        nav.setNavigationItemSelectedListener(new NavigationView.OnNavigationItemSelectedListener() {
+            @Override
+            public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+                item.setChecked(true);
+                int pos = items.indexOf(item);
+                if (pos == 0) {
+
+                } else if (pos == 1) {
+                    //Intent i = new Intent(agregarGasto.this, agregar_categoria.class);
+                    //startActivity(i);
+                } else if (pos == 2) {
+                    //Intent i = new Intent(agregarGasto.this, presupuestos.class);
+                    //startActivity(i);
+                } else if (pos == 3) {
+                    //Intent i = new Intent(agregarGasto.this, registros.class);
+                    //startActivity(i);
+                } else if (pos == 4) {
+                    //Intent i = new Intent(agregarGasto.this, acerca.class);
+                    //startActivity(i);
+                } else if (pos == 5) {
+                    Intent i = new Intent(Main.this, acerca.class);
+                    startActivity(i);
+                }
+                drawerLayout.closeDrawer(nav);
+                item.setChecked(false);
+                return false;
+            }
+        });
+
+        //Asignacion del header menu en una bariable
+        View headerview = nav.getHeaderView(0);
+        //Funcionalidad del boton de menu
+        btnMenu = (ImageView)findViewById(R.id.Btnmenu);
+        btnMenu.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                drawerLayout.openDrawer(nav);
+            }
+        });
+    }
+
+
+
 }
