5fe7713b4fe37d8a2064ca30b7817a8dad9bc3f3
Elias Rosales <mansion1@hotmail.com>
2017-05-14 10:27:16 -0500

    Interfaz Terminada

---
+++ app/src/main/java/com/claresti/obrasqro/agregar_punto_personal.java
@@ -0,0 +1,14 @@
+package com.claresti.obrasqro;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class agregar_punto_personal extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        getSupportActionBar().hide();
+        setContentView(R.layout.activity_agregar_punto_personal);
+    }
+}
---
+++ app/src/main/java/com/claresti/obrasqro/mostrar_puntos_personales.java
@@ -0,0 +1,13 @@
+package com.claresti.obrasqro;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class mostrar_puntos_personales extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_mostrar_puntos_personales);
+    }
+}
